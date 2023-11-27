2ae7ced319320e760558419e68bdd278ab370523
tedecti <wolftheguy08@gmail.com>
2023-11-27 10:07:35 +0600

    Main models

---
+++ Dentist API/Models/Gender.cs
@@ -0,0 +1,7 @@
+namespace Dentist_API.Models;
+
+public class Gender
+{
+    public int Id { get; set; }
+    public string GenderName { get; set; }
+}
---
+++ Dentist API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Dentist_API.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime Birthdate { get; set; }
+    public string Phone { get; set; }
+    public Gender Gender { get; set; }
+    public int GenderId { get; set; }
+}
