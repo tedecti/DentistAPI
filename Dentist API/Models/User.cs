8d99d932bbc305c9e407f109da2ecc23ca3a56c2
tedecti <wolftheguy08@gmail.com>
2023-11-27 10:13:22 +0600

    user model fix

--- Dentist API/Models/User.cs
+++ Dentist API/Models/User.cs
@@ -5,6 +5,7 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string Password { get; set; }
     public DateTime Birthdate { get; set; }
     public string Phone { get; set; }
     public Gender Gender { get; set; }
