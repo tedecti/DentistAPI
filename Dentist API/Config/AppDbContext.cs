3a91ed9027b50989c27bef160fe15830cae2e9e4
tedecti <wolftheguy08@gmail.com>
2023-11-27 10:08:07 +0600

    Initial migration and relationship

--- Dentist API/Config/AppDbContext.cs
+++ Dentist API/Config/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dentist_API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dentist_API.Config;
 
@@ -6,4 +7,13 @@ public class AppDbContext:DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=Dentist;Username=postgres;Password=123");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasOne(u=>u.Gender);
+    }
+
+    private DbSet<User> Users { get; set; }
+    private DbSet<Gender> Genders { get; set; }
 }
