20385be2a5bc6e89ab723de791b5ff3afc8b5564
tedecti <wolftheguy08@gmail.com>
2023-11-28 14:05:01 +0600

    Delete password from response

--- Dentist API/Dtos/Response/User/UserResponseDTO.cs
+++ Dentist API/Dtos/Response/User/UserResponseDTO.cs
@@ -4,7 +4,6 @@ public class UserResponseDTO
 {
     public string Name { get; set; }
     public string Surname { get; set; }
-    public string Password { get; set; }
     public DateTime Birthdate { get; set; }
     public string Phone { get; set; }   
     public int GenderId { get; set; }
