1c504095b2e525a017f823eace92dbf452139911
tedecti <wolftheguy08@gmail.com>
2023-12-01 22:04:43 +0600

    appointment model

---
+++ Dentist API/Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿namespace Dentist_API.Models;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public int DentistId { get; set; }
+    public Dentist Dentist { get; set; }
+    public DateTime AppointmentDate { get; set; }
+}
