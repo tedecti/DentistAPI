67b2f9dd1b8f68dc4e948417c1130830a5a0a555
tedecti <wolftheguy08@gmail.com>
2023-12-01 22:04:23 +0600

    dentist model

---
+++ Dentist API/Models/Dentist.cs
@@ -0,0 +1,9 @@
+﻿namespace Dentist_API.Models;
+
+public class Dentist
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Experience { get; set; }
+}
