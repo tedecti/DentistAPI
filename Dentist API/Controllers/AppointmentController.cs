b73b0e9b3a9f5c081302cfcb26b559d17f97a6b3
tedecti <wolftheguy08@gmail.com>
2023-12-02 11:39:42 +0600

    AppointmentController

---
+++ Dentist API/Controllers/AppointmentController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Dentist_API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Puppy.Dtos.Response.Appointment;
+
+namespace Dentist_API.Controllers
+{
+    [Route("api/appointment")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IAppointmentRepository _repository;
+
+        public AppointmentController(IMapper mapper, IAppointmentRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAppointment()
+        {
+            var user = Convert.ToInt32(HttpContext.User.Identity.Name);
+            var response = await _repository.GetAppointment(user);
+            var mappedResponse = _mapper.Map<AppointmentResponseDTO>(response);
+            return Ok(mappedResponse);
+        }
+
+        [HttpPost("{dentistId}")]
+        [Authorize]
+        public async Task<IActionResult> PostAppointment([FromBody] AppointmentRequestDTO appointmentRequestDto, int dentistId)
+        {
+            var user = Convert.ToInt32(HttpContext.User.Identity.Name);
+            await _repository.CreateAppointment(appointmentRequestDto, user, dentistId);
+            return StatusCode(201);
+        }
+    }
+}
