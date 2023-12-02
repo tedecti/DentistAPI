28100bde9d62d1f01264363988a064a24c338d5f
tedecti <wolftheguy08@gmail.com>
2023-12-02 11:39:25 +0600

    AppointmentRepository + interface

---
+++ Dentist API/Repositories/AppointmentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dentist_API.Config;
+using Dentist_API.Models;
+using Dentist_API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Puppy.Dtos.Response.Appointment;
+
+namespace Dentist_API.Repositories;
+
+public class AppointmentRepository : IAppointmentRepository
+{
+    private readonly AppDbContext _context;
+
+    public AppointmentRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Appointment> GetAppointment(int userId)
+    {
+        var appointment = await _context.Appointments.Where(a => a.UserId == userId).FirstOrDefaultAsync();
+        return appointment;
+    }
+
+    public async Task<Appointment> CreateAppointment(AppointmentRequestDTO requestDto, int userId, int dentistId)
+    {
+        var appointment = new Appointment
+        {
+            UserId = userId,
+            AppointmentDate = DateTime.UtcNow,
+            DentistId = dentistId
+        };
+        _context.Add(appointment);
+        await _context.SaveChangesAsync();
+        return appointment;
+    }
+}
---
+++ Dentist API/Repositories/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Dentist_API.Models;
+using Puppy.Dtos.Response.Appointment;
+
+namespace Dentist_API.Repositories.Interfaces;
+
+public interface IAppointmentRepository
+{
+    public Task<Appointment> GetAppointment(int userId);
+    public Task<Appointment> CreateAppointment(AppointmentRequestDTO requestDto, int userId, int dentistId);
+}
