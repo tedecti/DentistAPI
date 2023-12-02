d4c3326751abd63b910faffa2d409354d55d6d0a
tedecti <wolftheguy08@gmail.com>
2023-12-02 11:38:19 +0600

    AppointmentDTOs

---
+++ Dentist API/Dtos/Request/Appointment/AppointmentRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using Dentist_API.Models;
+
+namespace Puppy.Dtos.Response.Appointment;
+
+public class AppointmentRequestDTO
+{
+    public int UserId { get; set; }
+    public int DentistId { get; set; }
+    public DateTime AppointmentDate { get; set; }
+}
---
+++ Dentist API/Dtos/Response/Appointment/AppointmentResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿using Dentist_API.Models;
+
+namespace Puppy.Dtos.Response.Appointment;
+
+public class AppointmentResponseDTO
+{
+    public User User { get; set; }
+    public Dentist Dentist { get; set; }
+    public DateTime AppointmentDate { get; set; }
+}
