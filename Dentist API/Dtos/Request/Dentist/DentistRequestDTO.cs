c3fcbe302c84594f290e2947d98f628a34a94a25
tedecti <wolftheguy08@gmail.com>
2023-12-03 13:40:07 +0600

    DentistDTOs

---
+++ Dentist API/Dtos/Request/Dentist/DentistRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Puppy.Dtos.Request.Dentist;
+
+public class DentistRequestDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Experience { get; set; }
+}
---
+++ Dentist API/Dtos/Response/Dentist/DentistResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Puppy.Dtos.Request.Dentist;
+
+public class DentistResponseDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Experience { get; set; }
+}
