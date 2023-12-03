2f17fc92573d322f586d7d932483d7c0d5db8cd7
tedecti <wolftheguy08@gmail.com>
2023-12-03 13:41:04 +0600

    IDentistRepository

---
+++ Dentist API/Repositories/Interfaces/IDentistRepository.cs
@@ -0,0 +1,11 @@
+﻿using Dentist_API.Models;
+using Puppy.Dtos.Request.Dentist;
+
+namespace Dentist_API.Repositories.Interfaces;
+
+public interface IDentistRepository
+{
+    public Task<Dentist> GetDentists();
+    public Task<Dentist> GetDentist(int dentistId);
+    public Task<Dentist> CreateDentist(DentistRequestDTO requestDto);
+}
