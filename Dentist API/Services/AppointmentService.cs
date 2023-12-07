582c74f47de566b051a00eed9b329fa90fbbf6e1
tedecti <wolftheguy08@gmail.com>
2023-12-07 19:15:22 +0600

    AppointmentService + Interface

---
+++ Dentist API/Services/AppointmentService.cs
@@ -0,0 +1,23 @@
+﻿using Dentist_API.Config;
+using Dentist_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Puppy.Services.Interfaces;
+
+namespace Puppy.Services;
+
+public class AppointmentService : IAppointmentService
+{
+    private readonly AppDbContext _context;
+
+    public AppointmentService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Appointment> GetAppointment(int userId)
+    {
+        var appointment = await _context.Appointments.Include(a => a.User).Include(a => a.Dentist)
+            .Include(a => a.User.Gender).Where(a => a.UserId == userId).FirstOrDefaultAsync();
+        return appointment;
+    }
+}
---
+++ Dentist API/Services/Interfaces/IAppointmentService.cs
@@ -0,0 +1,8 @@
+﻿using Dentist_API.Models;
+
+namespace Puppy.Services.Interfaces;
+
+public interface IAppointmentService
+{
+    public Task<Appointment> GetAppointment(int userId);
+}
