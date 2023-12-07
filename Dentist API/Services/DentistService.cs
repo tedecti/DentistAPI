201230e7752ca37e90f7ee17ea54d5aa4c4813cf
tedecti <wolftheguy08@gmail.com>
2023-12-07 19:15:58 +0600

    DentistService + Interface

---
+++ Dentist API/Services/DentistService.cs
@@ -0,0 +1,28 @@
+﻿using Dentist_API.Config;
+using Dentist_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Puppy.Services.Interfaces;
+
+namespace Puppy.Services;
+
+public class DentistService : IDentistService
+{
+    private readonly AppDbContext _context;
+
+    public DentistService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Dentist>> GetDentists()
+    {
+        var dentists = await _context.Dentists.ToListAsync();
+        return dentists;
+    }
+
+    public async Task<Dentist> GetDentist(int dentistId)
+    {
+        var dentist = await _context.Dentists.Where(d => d.Id == dentistId).FirstOrDefaultAsync();
+        return dentist;
+    }
+}
---
+++ Dentist API/Services/Interfaces/IDentistService.cs
@@ -0,0 +1,9 @@
+﻿using Dentist_API.Models;
+
+namespace Puppy.Services.Interfaces;
+
+public interface IDentistService
+{
+    public Task<List<Dentist>> GetDentists();
+    public Task<Dentist> GetDentist(int dentistId);
+}
